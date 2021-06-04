641c16b3f934ff338ea87e8b64f631e588bf7b4f
MrBearWithHisSword <735683079@qq.com>
2021-06-04 18:53:55 +0800

    install oh-my-zsh with autosuggestions

---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# install plugins for oh-my-zsh
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i '/plugins=(/ s/(/(\
+zsh-autosuggestions\
+/' ~/.zshrc
+echo "bindkey ',' autosuggest-accept" >> ~/.zshrc
+
