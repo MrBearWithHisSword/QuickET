e156e451e8bba9b301a9b951e4b82a75d5ea8490
MrBearWithHisSword <735683079@qq.com>
2021-06-07 20:29:44 +0800

    mention pluings in ~/.zshrc

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,9 +2,11 @@
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 # install plugins for oh-my-zsh
 # zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i '/plugins=(/ s/(/(\
-zsh-autosuggestions\
-/' ~/.zshrc
-echo "bindkey ',' autosuggest-accept" >> ~/.zshrc
+ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed '/plugins=(/ s/(/(zsh-autosuggestions /' ~/.zshrc
+# sed -i '/plugins=(/ s/(/(\
+# zsh-autosuggestions\
+# /' ~/.zshrc
+echo "bindkey '+' autosuggest-accept" >> ~/.zshrc
+source ~/.zshrc
 
